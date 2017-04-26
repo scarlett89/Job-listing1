10d0eb91d99a1659d19b1e06d8de8ed1188d5782
tangjia <scarletttang89@163.com>
2017-04-26 20:41:53 +0800

    ch07 upload resume

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :jobs
-  root 'jobs#index'
 
   namespace :admin do
     resources :jobs do
@@ -12,4 +10,11 @@ Rails.application.routes.draw do
     end
   end
 end
+
+resources :jobs do
+  resources :resumes
+end
+
+root 'jobs#index'
+
 end
